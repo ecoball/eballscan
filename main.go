492983bdc38f8ebce339c0375bd3b21a1a93aa95
jatel <jatelmomo@hotmail.com>
2018-07-26 14:57:10 +0800

    add license

--- main.go
+++ main.go
@@ -1,3 +1,19 @@
+// Copyright 2018 The eballscan Authors
+// This file is part of the eballscan.
+//
+// The eballscan is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The eballscan is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the eballscan. If not, see <http://www.gnu.org/licenses/>.
+
 package main
 
 import (
@@ -5,8 +21,8 @@ import (
 	"html/template"
 	"net/http"
 
-	"github.com/ecoball/go-ecoball/explorer/data"
-	"github.com/ecoball/go-ecoball/explorer/onlooker"
+	"github.com/ecoball/eballscan/data"
+	"github.com/ecoball/eballscan/onlooker"
 )
 
 type WebHandle func(w http.ResponseWriter, r *http.Request)
--- onlooker/onlooker.go
+++ onlooker/onlooker.go
@@ -1,12 +1,28 @@
+// Copyright 2018 The eballscan Authors
+// This file is part of the eballscan.
+//
+// The eballscan is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The eballscan is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the eballscan. If not, see <http://www.gnu.org/licenses/>.
+
 package onlooker
 
 import (
 	"net"
 
+	"github.com/ecoball/eballscan/database"
 	"github.com/ecoball/go-ecoball/common"
 	"github.com/ecoball/go-ecoball/common/elog"
 	"github.com/ecoball/go-ecoball/core/types"
-	"github.com/ecoball/go-ecoball/explorer/database"
 	"github.com/ecoball/go-ecoball/spectator/info"
 )
 
